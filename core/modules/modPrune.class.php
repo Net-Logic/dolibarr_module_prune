44d0ca2cf77fcb26d1507f4ff9f6188a3dffb211
Frédéric FRANCE <frederic.france@free.fr>
2021-06-21 16:34:58 +0200

    add module def

--- core/modules/modPrune.class.php
+++ core/modules/modPrune.class.php
@@ -45,7 +45,7 @@ class modPrune extends DolibarrModules
 
 		// Id for module (must be unique).
 		// Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
-		$this->numero = 135999;
+		$this->numero = 135280;
 		// Key text used to identify module (for permissions, menus, etc...)
 		$this->rights_class = 'prune';
 		// Family can be 'base' (core modules),'crm','financial','hr','projects',
