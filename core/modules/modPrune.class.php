81a875793609b95f3f1ce852e1fd806a8166e075
Frédéric FRANCE <frederic.france@free.fr>
2021-11-03 18:32:16 +0100

    phpcs

--- core/modules/modPrune.class.php
+++ core/modules/modPrune.class.php
@@ -223,8 +223,7 @@ class modPrune extends DolibarrModules
 	 * @param   string  $options    Options when enabling module ('', 'noboxes')
 	 * @return  int                 1 if OK, 0 if KO
 	 */
-	public function remove($options = '')
-	{
+	public function remove($options= '') {
 		$sql = [];
 
 		return $this->_remove($sql, $options);
