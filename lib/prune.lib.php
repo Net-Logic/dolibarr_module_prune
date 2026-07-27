6101f0d59d4ef1e77b622a533b76d8097ad821f5
Frédéric FRANCE <frederic.france@free.fr>
2026-07-27 11:14:07 +0200

    fix retrieve token

--- lib/prune.lib.php
+++ lib/prune.lib.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (C) 2015-2023  Frédéric France      <frederic.france@netlogic.fr>
+ * Copyright (C) 2015-2026  Frédéric France      <frederic.france@netlogic.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
